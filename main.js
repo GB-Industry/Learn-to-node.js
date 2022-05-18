66b8a6aba8b64fcfc33d6e36fc82aa010281946b
GBIndustries <kkggbbpoint923@gmail.com>
2022-05-18 13:35:18 +0900

    modulizing

--- main.js
+++ main.js
@@ -2,57 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var template =require('./lib/template.js')
 
-function templateHTML(title,list,body, control){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB2 - ${title}</title>
-    <meta charset="utf-8">
-    <style media="screen">
-      body{
-        font-size: 30px;
-      }
-      .select{
-        font-size: 30px;
-        font-weight: 100;
-      }
-      .explain{
-        font-size: 20px;
-        font-weight: 100;
-      }
-      a{
-        text-decoration: none;
-        color: inherit;
-      }
-      .delete{
-        border: 5px;
-        font-size: 20px;
-        font-weight: 100;
-        color: red;
-      }
-    </style>
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
-}
-function templateLIST(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while(i < filelist.length){
-    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i++;
-  }
-  list += '</ul>';
-  return list;
-}
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -62,20 +13,20 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateLIST(filelist);
-          var template = templateHTML(title,list,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a style="color:blue;" href="/create">CREATE</a>`
           );
           response.writeHead(200);
-          response.end(template);
-        })
+          response.end(html);
+        });
       } else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var list = templateLIST(filelist);
-            var template = templateHTML(title,list,
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
               `<h2>${title}</h2>${description}`,
               `<a style="color:blue;"href="/create">CREATE</a>
                <a style="color:#fbbc05;"href="/update?id=${title}">UPDATE</a>
@@ -86,97 +37,100 @@ var app = http.createServer(function(request,response){
               `
             );
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
           });
         });
       }
     } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
-        var title = 'Web-create';
-        var list = templateLIST(filelist);
-        var template = templateHTML(title,list,`
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-          `,'');
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
         response.writeHead(200);
-        response.end(template);
-      })
+        response.end(html);
+      });
     } else if(pathname === '/create_process'){
-        var body ='';
-        request.on('data', function(data){
-          body += data;
-        });
-        request.on('end', function(){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
-          fs.writeFile(`data/${title}`,description, 'utf8', function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             response.writeHead(302, {Location: `/?id=${title}`});
-            response.end('success');
-          });
-        });
+            response.end();
+          })
+      });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
-          var list = templateLIST(filelist);
-          var template = templateHTML(title,list,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
             `
             <form action="/update_process" method="post">
             <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value = "${title}"></p>
+            <p>
+              <input type="text" name="title" placeholder="title" value = "${title}">
+            </p>
             <p>
               <textarea name="description" placeholder="description">${description}</textarea>
             </p>
             <p>
               <input type="submit" value="SUBMIT">
             </p>
-          </form>
+            </form>
             `,
-            `<a style="color:blue;" href="/create">CREATE</a>
-             <a style="color:#fbbc05;" href="/update?id=${title}">UPDATE</a>
-             `
+            `
+              <a style="color:blue;" href="/create">CREATE</a>
+              <a style="color:#fbbc05;" href="/update?id=${title}">UPDATE</a>
+            `
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
-      var body ='';
+      var body = '';
       request.on('data', function(data){
-        body += data;
+          body = body + data;
       });
       request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`,`data/${title}`,function(error){
-          fs.writeFile(`data/${title}`,description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
           });
-        })
       });
     } else if(pathname === '/delete_process'){
-      var body ='';
+      var body = '';
       request.on('data', function(data){
-        body += data;
+          body = body + data;
       });
       request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        fs.unlink(`data/${id}`, function(error){
-          response.writeHead(302, {Location: `/`});
-          response.end();
-        })
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
     } else {
       response.writeHead(404);
@@ -184,4 +138,3 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
-console.log('서버가 작동합니다.');
---
+++ nodejstutorial/object.js
@@ -0,0 +1,19 @@
+// var job = ['lawyer','programmer','diver'];
+// //console.log(job[3]);
+// var job2 = {
+//   'man1':'30',
+//   'man2':'56',
+//   'man3':'26',
+//   'man4':'28',
+//   'man5':'24'
+// }
+// for(var name in job2){
+//   console.log(name,'의 나이는',job2[name],'세 입니다.');
+// }
+// var add = function(a,b){
+//   return (a+b);
+// }
+function add(a,b){
+  return (a+b);
+}
+console.log(add(1023,125));
